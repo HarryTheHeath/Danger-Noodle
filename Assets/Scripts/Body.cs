04108c7fb3ef72809d8849fe3b4c03b5c8211b0b
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-01 18:56:14 +0100

    Updated Body.cs: removed awake method, fixed bug

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -1,19 +1,10 @@
- using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine;
 
 public class Body : MonoBehaviour
 {
     protected Body body;
-    private Transform playerTransform;
-    private Vector3 playerPos;
-    private Quaternion playerRot;
-
-
-    public void Awake()
-    {
-        playerTransform = transform;
-        playerPos = playerTransform.position;
-        playerRot = playerTransform.rotation;
-    }
 
     public void Grow(Body body)
     {
@@ -23,17 +14,17 @@ public class Body : MonoBehaviour
         }
         else
         {
-            this.body = Instantiate(body, playerPos, playerRot);
+            this.body = Instantiate(body, transform.position, transform.rotation);
         }
     }
 
     public void MoveTo(Vector3 position, Quaternion rotation)
     {
-        if (this.body != null)
+        if (body != null)
         {
-            this.body.MoveTo(playerPos, playerRot);
+            body.MoveTo(transform.position, transform.rotation);
         }
-        playerRot = rotation;
-        playerPos = position;
+        transform.rotation = rotation;
+        transform.position = position;
     }
 }
