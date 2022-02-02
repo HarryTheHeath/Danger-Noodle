c3ac2e677c44d9de06cdf59f1451cf29e83d8dc9
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-02 01:17:21 +0100

    Food.cs no longer spawns on snake body

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,9 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    public bool freeSpot { get; private set; }
+    private FoodSpawner foodSpawner;
+
+    private void Awake()
+    {
+        foodSpawner = GameObject.Find("Food Spawner").GetComponent<FoodSpawner>();
+    }
+    
     public void OnTriggerEnter2D(Collider2D col)
     {
+        if (col.GetComponent<Body>())
+        {
+            transform.position = foodSpawner.GetFoodSpawnPosition();
+        }
         var head = col.GetComponent<Head>();
         if (head == null)
         {
