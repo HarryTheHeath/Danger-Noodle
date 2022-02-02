a39cd6d45a694d151ffaba147fc667ca60e5bfab
HarryTheHeath <harry.heath@student.forsbergsskola.se>
2022-02-02 03:23:48 +0100

    Update Score.cs: Added TMP elements

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,8 +1,43 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
     public int PlayerScore { get; private set; } = 0;
+    private int HighScore = 0;
+    private int NewBest = 0;
+    public GameObject ScoreTMP;
+    public GameObject HighScoreTMP;
+
+    private TMP_Text scoreText;
+    private TMP_Text highScoreText;
+
+
+    private void Awake()
+    {
+        scoreText = ScoreTMP.GetComponent<TMP_Text>();
+        highScoreText = HighScoreTMP.GetComponent<TMP_Text>();
+    }
+
+    public void Update()
+    {
+        scoreText.text = $"Score: {PlayerScore}";
+
+        if (PlayerScore > HighScore)
+        {
+            HighScore = PlayerScore;
+        }
+        
+        if (HighScore > NewBest)
+        {
+            highScoreText.text = $"Best: {HighScore}";
+        }
+        else
+        {
+            highScoreText.text = $"Best: {NewBest}";
+        }
+    }
 
     public void AddScore()
     {
@@ -12,6 +47,10 @@ public class Score : MonoBehaviour
 
     public void ResetScore()
     {
+        if (HighScore > NewBest)
+        {
+            HighScore = NewBest;
+        }
         PlayerScore = 0;
     }
 }
